fb634765d9f0da0f092e85dfc20388ba808b0c19
Nishan It <nishan@m.insta.kim>
2020-12-31 19:32:02 +0600

    simple bug retry

--- app/components/withImageView/WithImageView.js
+++ app/components/withImageView/WithImageView.js
@@ -98,6 +98,7 @@ const WithImageView = ({
           renderItem={renderItem}
           keyExtractor={key}
           scrollEnabled={false}
+          showsHorizontalScrollIndicator={false}
           horizontal
         />
       </View>
